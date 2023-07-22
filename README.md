ccc5cf76dc11a5aed88982a503b681cfed92b8e7
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2023-07-22 16:17:55 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # chatgpt-terminal
+
 chatgpt-terminal
+
+## 1. Download required libraries
+
+```
+pip install -r requirements.txt
+
+```
+
+
+## 2. Add OpenAI API key
+
+Generate and copy a new OpenAI API key from API Key settings website: https://platform.openai.com/account/api-keys
+
+Put your new api key in a new file called ```.env```.
+
+```
+touch .env
+
+nano .env
+
+OPEN_AI_API_KEY=your-openai-api-key-goes-here
+```
+
+
+## 3. Copy files & add alias
+
+Copy gpt.py:
+```
+chmod +x gpt.py
+sudo cp gpt.py /usr/local/bin/gpt.py
+
+chmod +x .env
+sudo cp .env /usr/local/bin/.env
+```
+
+Add alias:
+``` 
+nano ~/.zshrc
+alias gpt="python gpt.py"
+source ~/.zshrc
+```
+
+## 4. Run 
+
+
+
+
+
---
+++ gpt.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+
+OPEN_AI_API_KEY = os.environ.get('OPEN_AI_API_KEY')
+OPEN_AI_API_URL = os.environ.get('OPEN_AI_API_URL')
+
+def chat_with_gpt3(prompt):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OPEN_AI_API_KEY}"
+    }
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": prompt}]
+    }
+
+    response = requests.post(OPEN_AI_API_URL, headers=headers, json=data)
+    if response.status_code == 200:
+        return response.json()["choices"][0]["message"]["content"]
+    else:
+        raise Exception("ChatGPT API request failed.")
+
+def main():
+    print("Welcome to ChatGPT! Type 'exit' to end the conversation.")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "exit":
+            break
+        response = chat_with_gpt3(user_input)
+        print("ChatGPT:", response)
+
+if __name__ == "__main__":
+    main()
